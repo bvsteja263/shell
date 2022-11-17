1c33889bf9756d11bbec31d969e10f6eb193f0fe
Venkata Sai Teja Bodapati <94819929+bvsteja263@users.noreply.github.com>
2022-11-17 18:28:43 +0530

    profiles

--- src/utils/bin/commands.ts
+++ src/utils/bin/commands.ts
@@ -243,12 +243,8 @@ export const explore = async (args: string[]): Promise<string> => {
 };
 
 export const profiles = async (args: string[]): Promise<string> => {
-  var links = config.profiles;
-
-for (var i = 0, linksLength = links.length; i < linksLength; i++) {
-  if (links[i].hostname != window.location.hostname) {
-    links[i].target = '_blank';
-  } 
-}
+  var links = [config.profile1 , config.profile2 , config.profile3 , config.profile4];
+  for (var i = 0; i < links.length; i++) {
+      window.open(links[i]);}
   return 'Opening profiles...';
 };
