b49fb7ef44d122c4e6d6532debf9c3d23450a0ea
Venkata Sai Teja Bodapati <94819929+bvsteja263@users.noreply.github.com>
2022-11-17 17:45:01 +0530

    profiles

--- src/utils/bin/commands.ts
+++ src/utils/bin/commands.ts
@@ -243,8 +243,9 @@ export const explore = async (args: string[]): Promise<string> => {
 };
 
 export const profiles = async (args: string[]): Promise<string> => {
-  var locs = ['https://pragnya.club/vboapati/','https://pragnya.club/rohinibarla/','https://pragnya.club/svattiku/','https://pragnya.club/skailash/'] 
-
-	for (let i = 0; i < locs.length; i++) {
-		window.open(locs[i]);}
-}
+  window.open(`${config.profile1}`);
+  window.open(`${config.profile2}`);
+  window.open(`${config.profile3}`);
+  window.open(`${config.profile4}`);
+  return 'Opening profiles...';
+};
